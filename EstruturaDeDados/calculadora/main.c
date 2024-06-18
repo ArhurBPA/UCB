70a7223ff5515819b2492f2154f22a740660aa5e
ArhurBPA <135137388+ArhurBPA@users.noreply.github.com>
2024-06-18 01:20:01 +0000

    commit

--- EstruturaDeDados/calculadora/main.c
+++ EstruturaDeDados/calculadora/main.c
@@ -2,13 +2,13 @@
 #include "calculadora.h"
 
 int main() {
-    char postfix[512] = "3 4 + 2 * 7 /";
+    char postfix[512] = "";
     float resultado = getValor(postfix);
     char *infixa = getFormaInFixa(postfix);
     infixa = removeParenteses(infixa);
 
     printf("Expressão Infixa: %s\n", infixa);
-    printf("Resultado: %.2f\n", resultado);
+    printf("Resultado: %f\n", resultado);
 
     return 0;
 }
